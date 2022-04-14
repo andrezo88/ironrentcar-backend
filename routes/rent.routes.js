f66b978c1edc8180b8cbcbedfb631b0c04be23f0
Eduardo Brandiao <eduardo.brandiao@al.infnet.edu.br>
2022-04-14 19:44:49 -0300

    routes done

--- routes/rent.routes.js
+++ routes/rent.routes.js
@@ -9,10 +9,9 @@ const router = Router();
 router.get("/", async (req, res) => {
     try {
         const userId = req.user.id;
-        // const userId = "6255d9314f9a3d577afc35f1"; // mocado
 
-        const allCars = await Rent.find({ user: userId })
-        res.status(200).json(allCars)
+        const allRents = await Rent.find({ user: userId })
+        res.status(200).json(allRents)
     } catch (error) {
         res.status(500).json({ error: error.message })
     }
@@ -24,46 +23,45 @@ router.post("/:carId", async (req, res) => {
         const userId = req.user.id;
         const { carId } = req.params;
         const { periodRent, payment } = req.body;
-        
+
         const car = await Car.findById(carId)
-        const valueCar = periodRent * car.value; 
+        const valueCar = periodRent * car.value;
 
         const newRent = await Rent.create({ ...req.body, user: userId, car: carId, value: valueCar, periodRent, payment });
         await User.findByIdAndUpdate(userId, { $push: { rent: newRent.id } });
         res.status(200).json(newRent);
     } catch (error) {
-        res.status(500).json( {error: error.message} )
+        res.status(500).json({ error: error.message })
     }
 });
 
 router.put("/:id", async (req, res) => {
     const { id } = req.params;
     const userId = req.user.id;
-  
-        // const userId = "6255d9314f9a3d577afc35f1"; // mocado
+
     try {
-        const updateCar = await Car.findByIdAndUpdate( {_id: id, user: userId}, req.body, {new: true,})
-        if(!updatedRent) {
+        const updateRent = await Rent.findByIdAndUpdate({ _id: id, user: userId }, req.body, { new: true, })
+        if (!updatedRent) {
             throw new Error("cannot update car from user")
         }
         res.status(200).json(updatedRent)
     } catch (error) {
-        res.status(500).json({ error: error.message})
+        res.status(500).json({ error: error.message })
     }
 });
 
 router.delete("/:id", async (req, res) => {
     const { id } = req.params;
-       const userId = req.user.id;
-        // const userId = "6255d9314f9a3d577afc35f1"; // mocado
+    const userId = req.user.id;
     try {
         const rent = await Rent.findById(id);
-        if(rent.user.toString() !== userId) {
-        throw new Error("Cannot delete")
-        } rent.delete();
+        if (rent.user.toString() !== userId) {
+            throw new Error("Cannot delete")
+        }
+        rent.delete();
         res.status(200).json();
     } catch (error) {
-        res.status(500).json( {error: error.message})
+        res.status(500).json({ error: error.message })
     }
 });
 module.exports = router;
